ad5100ba1271ac3105f2f3370e6dc7576f874665
praneethsai9 <53620074+praneethsai9@users.noreply.github.com>
2023-05-25 18:07:21 +0530

    sorting report

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 process.argv
 
 const {crawlPage}=require('./crawl.js')
+const {printReport}=require('./report.js')
 
 async function main(){
     if (process.argv.length<3){
@@ -14,9 +15,7 @@ async function main(){
 
     console.log(`starting crawl of ${baseURL}`)
     const pages = await crawlPage(baseURL,baseURL,{})
-    for(const page of Object.entries(pages)){
-        console.log(page)
-    }
+    printReport(pages)
 }
 
 main()
