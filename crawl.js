57c3a10429cd8c116c9f3ee4b1cc72820be2c846
praneethsai9 <53620074+praneethsai9@users.noreply.github.com>
2023-05-25 16:51:23 +0530

    crawl page single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,6 +1,35 @@
 const {JSDOM}=require('jsdom')
 
+async function crawlPage(currentURL){
 
+
+    console.log(`actively crawling ${currentURL}`)
+
+    try{
+
+        const resp =await fetch(currentURL)
+
+        if(resp.status>399){
+            console.log(`error in fetch with status code : ${resp.status} on page ${currentURL}`)
+            return
+        }
+
+        const contentType= resp.headers.get("content-type")
+
+        if (!contentType.includes("text/html")){
+            console.log(`non html response, content type :${contentType} on page: ${currentURL}`)
+            return 
+        }
+
+
+        console.log(await resp.text())
+
+   
+    }catch(err){
+        console.log("error in fetch")
+    }
+
+}
 
 function getURLsFromHTML(htmlBody,baseURL){
     const urls=[]
@@ -49,5 +78,6 @@ function normalizeURL(urlString){
 
 module.exports={
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Hi")
+process.argv
+
+const {crawlPage}=require('./crawl.js')
+
+function main(){
+    if (process.argv.length<3){
+        console.log("No website given")
+        process.exit(1)
+    }else if(process.argv.length>3){
+            console.log("Too many arguments")
+            process.exit(1)
+    }
+    const baseURL=process.argv[2]
+
+    console.log(`starting crawl of ${baseURL}`)
+    crawlPage(baseURL)
+}
+
+main()
